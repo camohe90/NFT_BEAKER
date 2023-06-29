f9a905ff4b5dbc9f3e4dd3f0b29a8bc62270c5cc
Camilo Molano <camilo@algorand.foundation>
2023-06-29 18:25:43 -0500

    NFT inner transaction Beaker

--- app.py
+++ app.py
@@ -13,10 +13,19 @@ def hello(name: abi.String, *, output: abi.String) -> Expr:
     return output.set(Concat(Bytes("Hello, "), name.get()))
 
 
-@app.delete(bare=True, authorize=Authorize.only(Global.creator_address()))
-def delete() -> Expr:
-    return Approve()
-
+@app.external
+def mint(name: abi.String, unit_name: abi.String, *, output: abi.String) -> Expr:
+     return InnerTxnBuilder.Execute(
+            {
+                TxnField.type_enum: TxnType.AssetConfig,
+                TxnField.config_asset_name: name.get(),
+                TxnField.config_asset_unit_name: unit_name.get(),
+                TxnField.config_asset_reserve: Global.current_application_address(),
+                TxnField.config_asset_manager: Global.current_application_address(),
+                TxnField.config_asset_url: Bytes("https://gateway.pinata.cloud/ipfs/QmcZyZ8KXSJomNBHS9QK4zMqH11dGZzQm3ye7Rtk8Qofys"),
+                TxnField.config_asset_total: Int(1),
+            }
+     )
 
 if __name__ == "__main__":
     root_path = Path(__file__).parent
